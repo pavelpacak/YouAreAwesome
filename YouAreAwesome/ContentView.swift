bd733b70ae6c3475bd7711f33215c44e3cb0739f
Pavel Pacák <pavel.pacak@gmail.com>
2025-02-22 18:27:24 +0100

    Changes after lesson 1.9: Intro to Arrays

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
+    @State private var messageNumber = 0
     @State private var imageName = ""
     @State private var imageNumber = 0
     var body: some View {
@@ -27,17 +28,26 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Show Message") {
-                let message1 = "You Are Awesome!"
-                let message2 = "You Are Great!"
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "Fabulous? That's You!",
+                                "You Are Fantastic!",
+                                "You Swifty!",
+                                "You Are a Code Monster!"]
                 
-                message = (message == message1 ? message2 : message1)
-
+                message = messages[messageNumber]
+                messageNumber += 1
+                
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
                 imageName = "image\(imageNumber)"
-                
                 imageNumber += 1
                 
                 if imageNumber > 9 {
